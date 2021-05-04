7265eb5087e24d4f5e5b5766e4a14f52a83765af
lhugens <leonardolobato@protonmail.com>
2021-05-04 12:43:11 +0100

    autocorrelation

---
+++ ising/correlation.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "mt19937.h"
+
+using namespace std;
+
+int main(){
+    dsfmt_seed(time(NULL));
+
+    // size of the array
+    int size = 6;
+    
+    // creating a random array just to test the correlation function
+    cout << "array:" << endl;
+    double m[size];
+    for(int i=0; i<size; i++){
+        m[i] =  dsfmt_genrand();
+        cout << m[i] << endl;
+    }
+
+    // this is the correlation calculation for a general_array "m" of length "size"
+    int t_max = size - 1;
+    double correlation[t_max]; 
+    double sum_correlation = 0;
+    double sum1, sum2, sum3, factor;
+
+    cout << "\ncorrelation:" << endl;
+    for(int t=0; t<t_max; t++){
+        sum1 = 0;
+        sum2 = 0;
+        sum3 = 0;
+
+        for(int s=0; s<t_max-t+1; s++){
+            sum1 += m[s] * m[s+t];
+            sum2 += m[s];
+            sum3 += m[s+t];
+        }
+
+        factor = (double)(t_max - t);
+        correlation[t] = (sum1 - sum2*sum3/factor) / factor;
+        sum_correlation += correlation[t];
+        cout << correlation[t] << endl;
+    }
+
+    cout << "\nestimate correlation time:" << endl;
+    cout << sum_correlation / correlation[0] << endl;
+
+
+
+}
