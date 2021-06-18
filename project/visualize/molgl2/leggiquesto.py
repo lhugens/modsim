77859a069aaf1a0b9c9c46e4daf313da957ae0cf
lhugens <leonardolobato@protonmail.com>
2021-06-18 21:50:04 +0200

    finally plotting is accurate

--- project/visualize/molgl2/leggiquesto.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3 
-
-import os
-import time
-we = "./a.out < leggimi.txt > spartaaa" 
-os.system(we)
--- project/visualize/molgl2/tentativo2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for k in {0..49};
-do
-a=$(($((2*$k))+1));
-echo $a;
-done
